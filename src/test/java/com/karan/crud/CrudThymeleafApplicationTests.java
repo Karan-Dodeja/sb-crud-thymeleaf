edc09bbfc53ea919a298b53fe10f0a445c5ab240
KaranDodeja03 <karandodeja03@gmail.com>
2022-09-28 00:26:41 +0530

    project created

---
+++ src/test/java/com/karan/crud/CrudThymeleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karan.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudThymeleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
